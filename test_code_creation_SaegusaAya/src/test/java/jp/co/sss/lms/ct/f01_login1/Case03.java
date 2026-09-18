2665a13596996e221559a5ea0df34655cfa48bb8
a-saegusa <asaegusa@system-west.co.jp>
2026-09-18 17:31:05 +0900

    add:Case03.java 受講生ログイン正常系テストコード追加
    WebElementを利用してログインID・パスワードを入力し、ログイン成功後の画面遷移を確認するテストを作成した。
    期待値は以下の通り
    
    テストNo.01：ログイン画面が表示され、ログインID・パスワード欄が空欄であること
    
    テストNo.02：コース詳細画面に遷移すること
    タイトル："コース詳細 | LMS"

--- test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,48 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルを取得
+		String title = webDriver.getTitle();
+		// ログインID欄・パスワード欄の要素を取得
+		final WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		final WebElement passwordElement = webDriver.findElement(By.id("password"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("ログイン | LMS", title, "タイトルが期待値通りであること");
+		assertEquals("", loginIdElement.getAttribute("value"), "ログインID欄が空欄であること");
+		assertEquals("", passwordElement.getAttribute("value"), "パスワード欄が空欄であること");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 初回ログイン済みの受講生ユーザーでログイン
+		final WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		final WebElement passwordElement = webDriver.findElement(By.id("password"));
+		loginIdElement.sendKeys("StudentAA01");
+		passwordElement.sendKeys("StudentAA01test");
+
+		// ログインボタンを押下する
+		webDriver.findElement(By.className("btn")).click();
+
+		// タイトルを取得
+		final String title = webDriver.getTitle();
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("コース詳細 | LMS", title, "コース詳細画面に遷移すること");
+
 	}
 
 }
