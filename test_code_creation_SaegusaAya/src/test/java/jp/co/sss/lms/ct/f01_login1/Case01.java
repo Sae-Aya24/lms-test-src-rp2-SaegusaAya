82c880584626417525d6451930b1a1a5919ccd49
a-saegusa <asaegusa@system-west.co.jp>
2026-09-18 13:47:27 +0900

    add:Case01.java ログイン画面への遷移テストコード追加
    WebElementを利用してログインID欄とパスワード欄の値を取得し、
    タイトルとあわせてそれぞれ期待値通りかどうか確認するテストを作成した。
    期待値は以下の通り
    タイトル：ログイン | LMS
    ログインID欄：空欄
    パスワード欄：空欄

--- test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +38,24 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルを取得
+		String title = webDriver.getTitle();
+
+		// ログインID欄、パスワード欄の要素を取得
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("ログイン | LMS", title, "タイトルが期待値通りであること");
+		assertEquals("", loginIdElement.getAttribute("value"), "ログインID欄が空欄であること");
+		assertEquals("", passwordElement.getAttribute("value"), "パスワード欄が空欄であること");
 	}
 
 }
