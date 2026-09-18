f01854789cf5c57e171e13b301a1349a956e80c9
a-saegusa <asaegusa@system-west.co.jp>
2026-09-18 14:51:18 +0900

    add:Case02.java 受講生ログイン認証失敗テストコード追加
    WebElementを利用してログインID・パスワード欄の値、およびログイン失敗時のエラーメッセージを取得し、それぞれ期待値通りか確認するテストを作成した。
    期待値は以下の通り
    
    (Case01同様)タイトル:"ログイン | LMS"(画面遷移なし)
    エラーメッセージ："ログインに失敗しました。"
    失敗後のログインID欄：unknownUser01(入力値がそのまま表示される)

--- test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,56 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルを取得
+		String title = webDriver.getTitle();
+
+		// ログインID欄、パスワード欄の要素を取得
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("ログイン | LMS", title, "タイトルが期待値通りであること");
+		assertEquals("", loginIdElement.getAttribute("value"), "ログインID欄が空欄であること");
+		assertEquals("", passwordElement.getAttribute("value"), "パスワード欄が空欄であること");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// DBに登録されていないログインID・パスワードを入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		loginIdElement.sendKeys("unknownUser01");
+		passwordElement.sendKeys("unknownUser01");
+
+		// ログインボタンを押下する
+		webDriver.findElement(By.className("btn-primary")).click();
+
+		// 他の画面に遷移していないことを確認するため、タイトルを取得
+		String title = webDriver.getTitle();
+
+		// 表示されるエラーメッセージを取得
+		WebElement errorElement = webDriver.findElement(By.className("help-inline"));
+
+		// 再表示されたログインID欄の要素を取得
+		WebElement reloadedLoginIdElement = webDriver.findElement(By.id("loginId"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("ログイン | LMS", title, "画面遷移せず、ログイン画面のままであること");
+		assertTrue(errorElement.getText().contains("ログインに失敗しました。"), "エラーメッセージ「ログインに失敗しました。」が表示されること");
+		assertEquals("unknownUser01", reloadedLoginIdElement.getAttribute("value"), "ログインID欄に入力した値がそのまま表示されていること");
 	}
 
 }
