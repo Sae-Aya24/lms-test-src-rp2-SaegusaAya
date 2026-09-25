439b0f4fceecf0e8472556e9ae867d3e40d17af5
a-saegusa <asaegusa@system-west.co.jp>
2026-09-25 17:21:13 +0900

    add:Case07.java 受講生レポート新規登録(日報)正常系テストコード追加
    WebElement、List、CSSセレクタ、拡張for文を利用し、
    ログインから未提出の日報の詳細確認・提出・提出確認までの画面遷移を確認するテストを作成した。
    期待値は以下の通り
    
    テストNo.01：ログイン画面が表示され、ログインID・パスワード欄が空欄であること
    
    テストNo.02：コース詳細画面に遷移すること
    タイトル："コース詳細 | LMS"
    
    テストNo.03：未提出の研修日の「詳細」ボタン押下でセクション詳細画面に遷移すること
    タイトル："セクション詳細 | LMS"
    
    テストNo.04：「日報【デモ】を提出する」ボタン押下でレポート登録画面に遷移すること
    タイトル："レポート登録 | LMS"
    
    テストNo.05：報告内容を入力し「提出する」ボタンを押下すると、セクション詳細画面に戻り、
    「本日のレポート」欄のボタン名が「提出済み日報【デモ】を確認する」に更新されること

--- test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_SaegusaAya/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +40,134 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// トップページURLにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		// タイトルを取得
+		String title = webDriver.getTitle();
+		// ログインID欄・パスワード欄の要素を取得
+		final WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		final WebElement passwordElement = webDriver.findElement(By.id("password"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("ログイン | LMS", title, "タイトルが期待値通りであること");
+		assertEquals("", loginIdElement.getAttribute("value"), "ログインID欄が空欄であること");
+		assertEquals("", passwordElement.getAttribute("value"), "パスワード欄が空欄であること");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 初回ログイン済みの受講生ユーザーでログイン
+		final WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		final WebElement passwordElement = webDriver.findElement(By.id("password"));
+		loginIdElement.sendKeys("StudentAA01");
+		passwordElement.sendKeys("StudentAA01test");
+
+		// ログインボタンを押下する
+		webDriver.findElement(By.className("btn")).click();
+
+		// コース詳細画面が表示されるまで待機(基準として「すべて開く」ボタンの表示を設定)
+		visibilityTimeout(By.id("open-all-panel"), 5);
+
+		// タイトルを取得
+		final String title = webDriver.getTitle();
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("コース詳細 | LMS", title, "コース詳細画面に遷移すること");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// コース詳細画面に表示されている研修日の行を全て取得
+		final List<WebElement> rows = webDriver.findElements(By.cssSelector(".sctionList tr"));
+
+		// 「未提出」の表示がある行から「詳細」ボタンを取得する
+		// 「詳細」ボタンを入れておく変数としてdetailButtonを用意
+		WebElement detailButton = null;
+		for (WebElement row : rows) {
+			if (row.getText().contains("未提出")) {
+				detailButton = row.findElement(By.cssSelector("input[value='詳細']"));
+				break;
+			}
+		}
+
+		// 「詳細」ボタンを押下する
+		detailButton.click();
+
+		// セクション詳細画面が表示されるまで待機(基準としてsection要素の表示を設定)
+		visibilityTimeout(By.id("section"), 5);
+
+		// タイトルを取得
+		final String title = webDriver.getTitle();
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("セクション詳細 | LMS", title, "セクション詳細画面に遷移すること");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		// 「(レポート名)を提出する」ボタンを取得
+		final WebElement submitButton = webDriver.findElement(By.cssSelector("input[value*='を提出する']"));
+
+		// 「(レポート名)を提出する」ボタンを押下する
+		submitButton.click();
+
+		// レポート登録画面が表示されるまで待機(基準として報告内容欄の表示を設定)
+		visibilityTimeout(By.cssSelector("[id^='content_']"), 5);
+
+		// タイトルを取得
+		final String title = webDriver.getTitle();
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertEquals("レポート登録 | LMS", title, "レポート登録画面に遷移すること");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		// 報告内容欄に入力値を入力する
+		// 入力値：本日はテストコード作成演習を行いました。
+		webDriver.findElement(By.cssSelector("[id^='content_']")).sendKeys("本日はテストコード作成演習を行いました。");
+
+		// 「提出する」ボタンを押下する
+		webDriver.findElement(By.className("btn")).click();
+
+		// セクション詳細画面に戻るまで待機(基準としてsection要素の表示を設定)
+		visibilityTimeout(By.id("section"), 5);
+
+		// 更新後の「本日のレポート」欄のボタンを取得
+		final WebElement submitButton = webDriver.findElement(By.cssSelector("input[value*='を確認する']"));
+
+		// エビデンスを取得
+		getEvidence(new Object() {
+		});
+
+		// 期待値と一致するか確認
+		assertTrue(submitButton.getAttribute("value").startsWith("提出済み"), "ボタン名が「提出済み(レポート名)を確認する」に更新されること");
 	}
 
 }
